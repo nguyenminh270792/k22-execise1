af1efea29d13294a6b41fc49ca24f826d381491a
mint.nguyen <nguyenminh2707@gmail.com>
2026-03-09 12:23:46 +0700

    feat: done ex3

---
+++ tests/lesson-03/ex3.js
@@ -0,0 +1,10 @@
+//check the odd or even number, from 1 to 100
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 2 == 0) {
+        console.log("Number", i, "is the even number");
+    }
+    else {
+        console.log("Number", i, "is the odd number");
+    }
+}
